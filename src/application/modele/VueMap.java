537ef9741de7dbe8c70c65bc93dda0ead3d66646
Adil Chetouani <chetouani.adil93@gmail.com>
2022-05-13 13:35:13 +0200

    Optimisation Affichage Map

--- src/application/modele/VueMap.java
+++ src/application/modele/VueMap.java
@@ -22,56 +22,27 @@ public class VueMap {
         for (int i=0; i < env.getNbTiles(); i++) {
             ImageView terre = new ImageView(tileset);
             ImageView ciel = new ImageView(tileset2);
-            switch(env.getCodeTiles(i)) {
-                case 0:
-                    ciel.setViewport(new Rectangle2D(0, 256, 32, 32));
-                    tilepane.getChildren().add(ciel);
-                    break;
-                case 1:
-                    terre.setViewport(new Rectangle2D(0, 0, 32, 32));
-                    tilepane.getChildren().add(terre);
-                    break;
-                case 2: 
-                    terre.setViewport(new Rectangle2D(32, 0, 32, 32));
-                    tilepane.getChildren().add(terre);
-                    break;
-                case 3:
-                    terre.setViewport(new Rectangle2D(64, 0, 32, 32));
-                    tilepane.getChildren().add(terre);
-                    break;
-                case 17:
-                    terre.setViewport(new Rectangle2D(0, 32, 32, 32));
-                    tilepane.getChildren().add(terre);
-                    break;
-                case 18:
-                    terre.setViewport(new Rectangle2D(32, 32, 32, 32));
-                    tilepane.getChildren().add(terre);
-                    break;
-                case 19:
-                    terre.setViewport(new Rectangle2D(64, 32, 32, 32));
-                    tilepane.getChildren().add(terre);
-                    break;
-                case 33:
-                    terre.setViewport(new Rectangle2D(0, 64, 32, 32));
-                    tilepane.getChildren().add(terre);
-                    break;
-                case 34:
-                    terre.setViewport(new Rectangle2D(32, 128, 32, 32));
-                    tilepane.getChildren().add(terre);
-                    break;
-                case 35: 
-                    terre.setViewport(new Rectangle2D(64, 64, 32, 32));
-                    tilepane.getChildren().add(terre);
-                    break;
-                default:
-                    break;
-            } 
+
+            int ligne = env.getCodeTiles(i)/16;
+            int colonne = (env.getCodeTiles(i)%16)-1;
+            System.out.print(ligne);
+            System.out.println(colonne);
+            int y = ligne * 32;
+            int x = colonne * 32;
+
+            if (env.getCodeTiles(i) != 0) {
+                terre.setViewport(new Rectangle2D(x, y, 32, 32));
+                tilepane.getChildren().add(terre);
+            } else {
+                ciel.setViewport(new Rectangle2D(0, 256, 32, 32));
+                tilepane.getChildren().add(ciel);
+            }
+
+            }
         }
-    }
-
-
-
-
-
-
 }
+
+
+
+
+
