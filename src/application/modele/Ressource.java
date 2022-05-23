31ae483d3f2a9e5385d5f8121ecbe0f26986b589
Adil Chetouani <chetouani.adil93@gmail.com>
2022-05-23 13:06:21 +0200

    Initialisation de la classe ressource

--- src/application/modele/Ressource.java
+++ src/application/modele/Ressource.java
@@ -1,4 +1,19 @@
 package application.modele;
 
 public class Ressource {
+    private int id;
+    private String nom;
+
+    public Ressource(int id,String nom) {
+        this.id=id;
+        this.nom=nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
 }
