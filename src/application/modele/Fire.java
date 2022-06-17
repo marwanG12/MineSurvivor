e3f46e48a36f85d3b944709e3b926f1b45204351
Adil Chetouani <chetouani.adil93@gmail.com>
2022-06-17 08:24:10 +0200

    affichage boule de feu

--- src/application/modele/Fire.java
+++ src/application/modele/Fire.java
@@ -1,25 +1,29 @@
 package application.modele;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 
 public class Fire {
 
-    private static String url = "application/images/fire.png";
+    private String url = "application/images/fire.png";
     private static int count=0;
     private int id;
-    private IntegerProperty x,y;
+    private IntegerProperty x, y;
     private int width=32, height=32;
     private Environnement env;
     private Necromancer necromancer;
     private double degat;
     private boolean right = false, left = false;
-    private boolean touch;
+    private boolean active = false;
+    private static int loop = 0;
 
     public Fire(Necromancer necromancer, double degat, Environnement env) {
         this.id = count++;
         this.necromancer = necromancer;
         this.env = env;
         this.degat = degat;
+        this.x = new SimpleIntegerProperty(necromancer.getX());
+        this.y = new SimpleIntegerProperty(necromancer.getY());
     }
 
     public int getWidth() { return width; }
@@ -44,10 +48,14 @@ public class Fire {
 
     public int getId() { return id; }
 
-    public static String getUrl() { return url; }
+    public String getUrl() { return url; }
+
+    public boolean isActive() { return active; }
+
+    public void setActive(boolean active) { this.active = active; }
 
     public Entite checkZone() {
-        if ((this.getX() - 12 <= env.getJoueur().getX()) && (env.getJoueur().getX() <= this.getX() + 12)  && (env.getJoueur().getY() == this.getY())) {
+        if ((this.getX() - 5 <= env.getJoueur().getX()) && (env.getJoueur().getX() <= this.getX() + width)  && (env.getJoueur().getY() == this.getY())) {
             return env.getJoueur();
         }
         return null;
@@ -64,17 +72,34 @@ public class Fire {
     }
 
     public void agit() {
-        x = necromancer.getXProperty();
-        y = necromancer.getYProperty();
-        Entite e = this.checkZone();
-        if (e instanceof Joueur) {
-            env.getFires().remove(this);
-            env.getJoueur().decrementerPv(degat);
-            if (env.getJoueur().getPv() == 0) {
-                env.getJoueur().meurt();
+        if (active) {
+            Entite e = this.checkZone();
+            if (e instanceof Joueur) {
+                env.getJoueur().decrementerPv(degat);
+                if (env.getJoueur().getPv() == 0) {
+                    env.getJoueur().meurt();
+                }
+                active = false;
+                count--;
+                env.getFires().remove(this);
+            } else if (e == null) {
+                loop++;
+                seDeplace();
+                System.out.println("x" + x);
+                System.out.println("y" + y);
+                if (loop != 10) {
+                    active = false;
+                    loop = 0;
+                    count--;
+                    env.getFires().remove(this);
+                }
             }
-        } else if (e == null) {
-            seDeplace();
         }
     }
+
+    public static int getLoop() {
+        return loop;
+    }
+
+    
 }
