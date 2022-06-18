123ac399454491c723983d89c3cd40d9f51166b1
Marwan <mght21@hotmail.com>
2022-06-18 22:02:55 +0200

    Création du Folder test, de la classe InventaireTest + création des méthodes de test (pas encore complètes)

---
+++ test/application/modele/InventaireTest.java
@@ -0,0 +1,46 @@
+package application.modele;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventaireTest {
+
+    @org.junit.jupiter.api.Test
+    void getCurrentItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItems() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRessources() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isSelect() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isCraft() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addRessource() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void craftItem() {
+    }
+}
