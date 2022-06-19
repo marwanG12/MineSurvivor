639ec03bb0bd1d5e968ea9780a7e7b277feaaae5
Marwan <mght21@hotmail.com>
2022-06-19 13:30:03 +0200

    Actualisation de la classe InventaireTest : succès des test pour getCurrentItem, getItems, getRessources, removeItem.

--- test/application/modele/InventaireTest.java
+++ test/application/modele/InventaireTest.java
@@ -1,19 +1,35 @@
 package application.modele;
 
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InventaireTest {
 
     @org.junit.jupiter.api.Test
-    void getCurrentItem() {
+    void getCurrentItem() throws Exception {
+        Inventaire inv = new Inventaire();
+        inv.addItem(new Pioche("testPioche", 0.5, 1));
+        inv.addItem(new Epee("testEpee"));
+        inv.setCurrentItem(inv.getItems().get(2));
+        Assertions.assertEquals(inv.getItems().get(2),inv.getCurrentItem());
     }
 
     @org.junit.jupiter.api.Test
-    void getItems() {
+    void getItems() throws Exception {
+        Inventaire inv = new Inventaire(); /*Lors de la création de l'inventaire du joueur il y a par defaut une pioche*/
+        inv.addItem(new Pioche("testPioche", 0.5, 1));
+        inv.addItem(new Epee("testEpee"));
+        inv.addItem(new Potion("potionTest"));
+        Assertions.assertEquals(4,inv.getItems().size());
     }
 
     @org.junit.jupiter.api.Test
     void getRessources() {
+        Inventaire inv = new Inventaire();
+        inv.addRessource(0/*pierre*/);
+        Assertions.assertEquals(1,inv.getRessources().get(0).getNombre());
+
     }
 
     @org.junit.jupiter.api.Test
@@ -28,12 +44,19 @@ class InventaireTest {
     void checkId() {
     }
 
-    @org.junit.jupiter.api.Test
+    @org.junit.jupiter.api.Test //ne sert a rien de le tester
     void addRessource() {
+
     }
 
     @org.junit.jupiter.api.Test
-    void removeItem() {
+    void removeItem() throws Exception {
+        Inventaire inv = new Inventaire();
+        inv.addItem(new Epee("épéeTest"));
+        inv.addItem(new Pioche("piocheTest",0.5,1));
+        inv.selectItem(inv.getItems().get(0),false);
+        inv.removeItem();
+        Assertions.assertEquals(2,inv.getItems().size());
     }
 
     @org.junit.jupiter.api.Test
